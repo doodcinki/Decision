16c398be244129e0df1c10e43533db678a4cbe8a
doodcinki <doodcinki@gmail.com>
2022-05-27 12:54:40 +0100

    Add project files.

---
+++ Decision/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Decision
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        /*  {
+              Console.WriteLine("Szymona Rozdawanie");
+              Console.WriteLine("Wybierz drzwi: 1, 2, lub 3: ");
+              string userValue = Console.ReadLine();
+
+              string message = "";
+
+              if (userValue == "1")
+
+                   message = "Wygrales talon";
+              else if (userValue == "2")
+                   message = "Wygrales Balon";
+              else if (userValue == "3")
+                   message = "Wygrales taczke gruzu";
+
+              else
+              {
+                  message = "Nawet nie umiesz wcisnac odpowiedniego. ";
+                  message = message + "Milego dnia! ";
+                   }
+              Console.WriteLine(message);
+              Console.ReadLine();
+        */
+        {
+            Console.WriteLine("Szymona Rozdawanie");
+            Console.WriteLine("Wybierz drzwi: 1, 2, lub 3: ");
+            string userValue = Console.ReadLine();
+
+            string message = (userValue == "1") ? "Wygrales Balon" : "wygrales taczke gruzu";
+            //Console.Write("Czy ");
+            //Console.Write(message);
+            //Console.Write(".");
+            //Console.ReadLine();
+
+            Console.WriteLine("Wybrales: {0}., Jednakze {1}. ", userValue, message);
+            Console.ReadLine();
+        }
+    }
+}
